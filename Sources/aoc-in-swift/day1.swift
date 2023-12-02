1af7f085618d112e509c4bedea8d11a78d5f0be9
Brian Cooke <cookebri@gmail.com>
2023-12-02 13:04:52 -0800

    first commit + 2023, day 1

---
+++ Sources/aoc-in-swift/day1.swift
@@ -0,0 +1,44 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import Foundation
+
+public struct Day1 {
+    public func part1(input: String) -> String {
+        return String(
+            input.split(separator: "\n").map { line in
+                let characterSet = CharacterSet.letters
+                let components = line.components(separatedBy: characterSet)
+                let justDigits = components.joined(separator: "")
+                guard let first = justDigits.first,
+                    let last = justDigits.last,
+                    let result = Int((String(first) + String(last)))
+                else {
+                    fatalError()
+                }
+                return result
+            }.reduce(0, +))
+    }
+
+    public func part2(input: String) -> String {
+        let numbersAsWords = [
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ]
+
+        return self.part1(
+            input: input.split(separator: "\n").map { line in
+                var digitsOnly = ""
+                for i in 0..<line.count {
+                    let start = line.startIndex + i
+                    for (numberIndex, word) in numbersAsWords.enumerated() {
+                        if line.suffix(from: start).first!.isNumber == true {
+                            digitsOnly += String(line.suffix(from: start).first!)
+                        }
+                        if line[start...].starts(with: word) {
+                            digitsOnly += String(numberIndex)
+                        }
+                    }
+                }
+                return digitsOnly
+            }.joined(separator: "\n"))
+    }
+}
---
+++ Tests/aoc-in-swiftTests/day1tests.swift
@@ -0,0 +1,46 @@
+import XCTest
+
+@testable import aoc_in_swift
+
+final class aoc_in_swiftTests: XCTestCase {
+    let input = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+        """
+
+    let part2input = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+        """
+
+    func testPart1Example() throws {
+        XCTAssertEqual("142", Day1().part1(input: input))
+    }
+
+    func testPart1Input() throws {
+        guard let file = Bundle.module.path(forResource: "day1", ofType: "txt") else {
+            fatalError()
+        }
+        let input = try! String.init(contentsOfFile: file)
+        print("Day 1, part 1 \(Day1().part1(input: input))")
+    }
+
+    func testPart2Example() throws {
+        XCTAssertEqual("281", Day1().part2(input: part2input))
+    }
+
+    func testPart2Input() throws {
+        guard let file = Bundle.module.path(forResource: "day1", ofType: "txt") else {
+            fatalError()
+        }
+        let input = try! String.init(contentsOfFile: file)
+        print("Day 1, part 2 \(Day1().part2(input: input))")
+    }
+}

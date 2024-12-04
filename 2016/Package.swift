eb0ddb8b2c947ce8084db861bbf22db02c2cf566
Brian Cooke <cookebri@gmail.com>
2024-12-03 19:41:20 -0800

    hello 2016

---
+++ 2016/Package.swift
@@ -0,0 +1,49 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let dependencies: [Target.Dependency] = [
+    .product(name: "Algorithms", package: "swift-algorithms"),
+    .product(name: "Collections", package: "swift-collections"),
+    .product(name: "ArgumentParser", package: "swift-argument-parser"),
+    .product(name: "Parsing", package: "swift-parsing"),
+]
+
+let package = Package(
+    name: "AdventOfCode",
+    platforms: [.macOS(.v13)],
+    dependencies: [
+        .package(
+            url: "https://github.com/apple/swift-algorithms.git",
+            .upToNextMajor(from: "1.2.0")
+        ),
+        .package(
+            url: "https://github.com/apple/swift-collections.git",
+            .upToNextMajor(from: "1.0.0")
+        ),
+        .package(
+            url: "https://github.com/apple/swift-argument-parser.git",
+            .upToNextMajor(from: "1.2.0")
+        ),
+        .package(
+            url: "https://github.com/apple/swift-format.git",
+            .upToNextMajor(from: "509.0.0")
+        ),
+        .package(
+            url: "https://github.com/pointfreeco/swift-parsing",
+            .upToNextMajor(from: "0.13.0")
+        ),
+    ],
+    targets: [
+        .executableTarget(
+            name: "AdventOfCode",
+            dependencies: dependencies,
+            resources: [.copy("Data")],
+            swiftSettings: [.enableUpcomingFeature("BareSlashRegexLiterals")]
+        ),
+        .testTarget(
+            name: "AdventOfCodeTests",
+            dependencies: ["AdventOfCode"] + dependencies,
+            swiftSettings: [.enableUpcomingFeature("BareSlashRegexLiterals")]
+        ),
+    ]
+)
---
+++ 2016/Templates/Solution.swift
@@ -0,0 +1,13 @@
+import Algorithms
+
+struct Day00: AdventDay {
+    var data: String
+
+    func part1() throws -> Any {
+        throw AdventError.notImplemented
+    }
+
+    func part2() throws -> Any {
+        throw AdventError.notImplemented
+    }
+}

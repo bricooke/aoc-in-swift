1c9566ec2601f230d75653995a316992a81e3d03
Brian Cooke <cookebri@gmail.com>
2025-12-01 15:47:40 -0800

    2025 setup

---
+++ 2025/Sources/Day01.swift
@@ -0,0 +1,13 @@
+import Algorithms
+
+struct Day01: AdventDay {
+    var data: String
+
+    func part1() throws -> Any {
+        throw AdventError.notImplemented
+    }
+
+    func part2() throws -> Any {
+        throw AdventError.notImplemented
+    }
+}
---
+++ 2025/Templates/Test.swift
@@ -0,0 +1,52 @@
+//
+//  Test.swift
+//  AdventOfCode
+//
+//  Created by Brian Cooke on 11/16/24.
+//
+
+import Testing
+
+@testable import AdventOfCode
+
+struct Day00Tests {
+    @Test(
+        "part1",
+        arguments:
+            zip(
+                [
+                    """
+                    """
+                ],
+                [
+                    0
+                ]
+            )
+    ) func part1(data: String, expected: Int) throws {
+        let challenge = Day00(data: data)
+        #expect(
+            String(describing: try challenge.part1())
+                == String(describing: expected)
+        )
+    }
+
+    @Test(
+        "part2",
+        arguments:
+            zip(
+                [
+                    """
+                    """
+                ],
+                [
+                    0
+                ]
+            )
+    ) func part2(data: String, expected: Int) throws {
+        let challenge = Day00(data: data)
+        #expect(
+            String(describing: try challenge.part2())
+                == String(describing: expected)
+        )
+    }
+}
